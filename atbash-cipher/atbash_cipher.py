0f57a9acf34c4e840e3ba01e8c840313a237a9aa
Saarthak Gupta <saarthak.gupta@chicagobooth.edu>
2019-09-18 16:22:08 +0000

    modified atbash

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,5 +1,20 @@
+
+master_string = "abcdefghijklmnopqrstuvwxyz"
+
+print(master_string[3])
+    
+    
 def encode(plain_text):
-    pass
+    
+    word_out = ""
+    
+    for letter in plain_text:
+        letter_index = master_string.find(letter)
+        new_letter = master_string[(len(master_string)-letter_index-1)]
+        word_out = word_out.join(new_letter)
+    
+    return(word_out)
+    
 
 
 def decode(ciphered_text):
