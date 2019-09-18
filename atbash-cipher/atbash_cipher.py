4638b859c3d554f69f0007b9826abb3eccf5efcb
Saarthak Gupta <saarthak.gupta@chicagobooth.edu>
2019-09-18 18:51:41 +0000

    SG post atbash cipher

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,21 +1,40 @@
 
 master_string = "abcdefghijklmnopqrstuvwxyz"
-
-print(master_string[3])
-    
     
 def encode(plain_text):
     
+    plain_text_out = plain_text.lower()
+    
     word_out = ""
     
-    for letter in plain_text:
-        letter_index = master_string.find(letter)
-        new_letter = master_string[(len(master_string)-letter_index-1)]
-        word_out = word_out.join(new_letter)
-    
-    return(word_out)
+    for letter in plain_text_out:
+        try:
+            letter_index = master_string.find(letter)
+            new_letter = master_string[(len(master_string)-letter_index-1)]
+            word_out = word_out + new_letter
+        except:
+            pass
+        
+    format_word_out = " ".join([word_out[i:i+5] for i in range(0, len(word_out), 5)])
+        
+    return(format_word_out)
     
 
 
 def decode(ciphered_text):
-    pass
+    
+    word_out = ""
+    
+    ciphered_edit = ciphered_text.replace(" ","")
+    
+    for letter in ciphered_edit:
+        try:
+            letter_index = master_string.find(letter)
+            new_letter = master_string[(len(master_string)-letter_index-1)]
+            word_out = word_out + new_letter
+        except:
+            pass
+        
+    return(word_out)
+    
+    
