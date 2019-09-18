bc8c141f0d85ff999570a6b35f52700f85c85559
Derek Miller <dgmxm7@gmail.com>
2019-09-18 15:10:54 +0000

    Finished scrabble_score.py. Tested on input cabbage == 14

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,26 @@
 def score(word):
-    pass
+    
+    # ensure word is uppercase
+    word = word.upper()
+    
+    # initialize score at zero
+    score = 0
+    for letter in word:
+        if letter in 'AEIOULNRST':
+            score += 1
+        elif letter in 'DG':
+            score += 2
+        elif letter in 'BCMP':
+            score += 3
+        elif letter in 'FHVWY':
+            score += 4
+        elif letter in 'K':
+            score += 5
+        elif letter in 'JX':
+            score += 8
+        elif letter in 'QZ':
+            score += 10
+        else:
+            raise ValueError("Input {0} is not a letter".format(letter))
+        
+    return score
