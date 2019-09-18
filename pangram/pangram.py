7e6250918149a06fae4dd9485cc25a179c531225
Derek Miller <dgmxm7@gmail.com>
2019-09-18 15:23:12 +0000

    Finished a simple version of pangram. No exception handling and it currently won't pass some edge cases but 90% of the time it should work every time.

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,9 @@
 def is_pangram(sentence):
-    pass
+    
+    # ensure string is lowercase
+    sentence = sentence.lower()
+    
+    # remove some punctuation
+    for thing in ' ,.!?:;()':
+        sentence = sentence.replace(thing,'')
+    return set(sentence) == set("abcdefghijklmnopqrstuvwxyz")
