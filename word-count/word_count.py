9fc94e2fa213ae35f667ae14fc73273fe06792dd
Pietro Ramella <pietro.ramella@chicagobooth.edu>
2019-09-18 19:55:05 +0000

    Fixed problem of word-count: \w matches also underscores and asterix, not just alphabetical characters

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,7 +1,7 @@
 import re
 def count_words(sentence):
     #[\s,.:;!?\']?
-    list_w = [item.group(1).lower() for item in re.finditer(r'\b((\w+(\'\w+)?|[0-9]+))',sentence)]
+    list_w = [item.group(1).lower() for item in re.finditer(r'(([A-Za-z]+(\'[A-Za-z]+)?|[0-9]+))',sentence)]
     dictCount = {}
     for item in list_w:
         if item in dictCount:
