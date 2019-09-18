a85238a871edffcbf007dcd002179abae9faca77
Pietro Ramella <pietro.ramella@chicagobooth.edu>
2019-09-18 19:45:42 +0000

    Fixed case insesitivity and self word not a anagram issues

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,12 +1,14 @@
 def find_anagrams(word, candidates):
     is_anagram = []
     for c in candidates:
+        if c.lower() == word.lower():
+            continue
         if len(c) == len(word):
             list_c = []
             list_w = []
             for w in range(len(word)):
-                list_c.append(c[w])
-                list_w.append(word[w])
+                list_c.append(c[w].lower())
+                list_w.append(word[w].lower())
             if sorted(list_c) == sorted(list_w):
                 is_anagram.append(c)
     return(is_anagram)
