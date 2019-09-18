900c629ba727fb018b7c65cd78ef1e3897b92f6a
Pietro Ramella <pietro.ramella@chicagobooth.edu>
2019-09-18 16:16:07 +0000

    word-count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,11 @@
+import re
 def count_words(sentence):
-    pass
+    #[\s,.:;!?\']?
+    list_w = [item.group(1).lower() for item in re.finditer(r'\b(([\w]+(\'s)?|[0-9]+))',sentence)]
+    dictCount = {}
+    for item in list_w:
+        if item in dictCount:
+            dictCount[item] += 1
+        else:
+            dictCount[item] = 1
+    return(dictCount)
