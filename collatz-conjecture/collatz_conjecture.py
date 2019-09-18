599a6d302e9266b4cb9b22ed5746acce991cde20
Saarthak Gupta <saarthak.gupta@chicagobooth.edu>
2019-09-18 15:19:55 +0000

    SG post collatz

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,11 @@
 def steps(number):
-    pass
+    n = 0
+    
+    while number != 1:
+        if number % 2 == 0:
+            number = number/2
+        else:
+            number = number*3 + 1
+        n = n + 1
+    
+    return(n)
--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,5 @@
 def is_isogram(string):
-    pass
+    string1 = string.replace("-", "")
+    newstring = string.replace(" ", "")
+    return(newstring[i] for i in range(0:len(newstring)))
+           
