1d1ceda537204f0e0e5f31c0a255f05f1cbbb44e
Paulo Henrique Ramos <paulohenrique.ramos@chicagobooth.edu>
2019-09-18 21:22:22 +0000

    Fixed the problems with the unit tests

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,8 +1,12 @@
+import re
 def abbreviate(words):
     if type(words)!=str:
         return("input should be a string")
     else:
+        words = re.sub("[-_,]", " ", words)
+        print(words)
         tokens = words.split()
         acronym = [token[0] for token in tokens]
         acronym = "".join(acronym)
+        acronym = acronym.upper()
         return(acronym)
--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,20 +1,26 @@
+import re
 def rotate(text, key):
     if type(text)!=str:
         return('first input should be a string')
     else:
-        text = text.lower()
         alphabet = 'abcdefghijklmnopqrstuvwxyz'
-        tempList = []
-        count = 0
-        for letter in alphabet:
-            tempList.append((letter,count))
-            count += 1
-        lettersDict = dict(tempList)
+        ALPHABET = alphabet.upper()
+        lowDict = []
+        uppDict = []
+        for ind in range(len(alphabet)):
+            lowDict.append((alphabet[ind],ind))
+            uppDict.append((ALPHABET[ind],ind))
+        lowDict = dict(lowDict)
+        uppDict = dict(uppDict)
         newText = ""
         for letter in text:
-            if letter==" ":
+            if re.match('(?![a-zA-Z])',letter):
                 newText += letter
             else:
+                if letter.isupper():
+                    lettersDict = uppDict
+                else:
+                    lettersDict = lowDict
                 newKey = lettersDict.get(letter)
                 if newKey + key <= 25:
                     newKey += key
@@ -23,9 +29,3 @@ def rotate(text, key):
                 newLetter = list(lettersDict.keys())[list(lettersDict.values()).index(newKey)]
                 newText += newLetter
         return(newText)
-
-print(rotate('omg', 5))
-print(rotate('c', 0))
-print(rotate('Cool', 26))
-print(rotate('The quick brown fox jumps over the lazy dog', 13))
-print(rotate('Gur dhvpx oebja sbk whzcf bire gur ynml qbt', 13))
