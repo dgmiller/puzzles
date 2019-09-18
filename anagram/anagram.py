acfd896e55486bca258bb08c1c71b601268f3d69
Pietro Ramella <pietro.ramella@chicagobooth.edu>
2019-09-18 15:46:54 +0000

    Solved anagram and leap

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,12 @@
 def find_anagrams(word, candidates):
-    pass
+    is_anagram = []
+    for c in candidates:
+        if len(c) == len(word):
+            list_c = []
+            list_w = []
+            for w in range(len(word)):
+                list_c.append(c[w])
+                list_w.append(word[w])
+            if sorted(list_c) == sorted(list_w):
+                is_anagram.append(c)
+    return(is_anagram)
--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,15 @@
 def leap_year(year):
-    pass
+    is_leap = 0
+    if year%4==0:
+        if year%100!=0:
+            is_leap = 1
+        else:
+            if year%400==0:
+                is_leap = 1
+    if is_leap == 1:
+        print(year,"is a leap year")
+    else:
+        print(year,"is not a leap year")
+
+
+
