6def0dad7e0f04a41e7be5dce4bf110f2ecb2631
Saarthak Gupta <saarthak.gupta@chicagobooth.edu>
2019-09-18 15:31:23 +0000

    SG post isogram

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,5 +1,13 @@
 def is_isogram(string):
     string1 = string.replace("-", "")
-    newstring = string.replace(" ", "")
-    return(newstring[i] for i in range(0:len(newstring)))
-           
+    string2 = string1.replace(" ", "")
+    newstring = string2.lower()
+    
+    maxn = 0
+    
+    for letter in newstring:
+        newn = newstring.count(letter)
+        maxn = max(newn,maxn)
+        
+    return(maxn)
+        
