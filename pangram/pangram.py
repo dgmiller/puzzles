d4abe10d025872af22478ef656f5a99a4fc64be4
Derek Miller <dgmxm7@gmail.com>
2019-09-18 18:33:22 +0000

    Added $ symbol to list of things to replace and added a docstring.

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,9 +1,17 @@
 def is_pangram(sentence):
+    """
+    Tests to see if input sentence is a pangram (a sentence containing all letters of the alphabet).
+    INPUT
+        sentence (str)
+    RETURNS
+        (bool) indicating whether the sentence is a pangram
+    
+    """
     
     # ensure string is lowercase
     sentence = sentence.lower()
     
     # remove some punctuation
-    for thing in ' ,.!?:;()':
+    for thing in ' ,.!?:;()$':
         sentence = sentence.replace(thing,'')
     return set(sentence) == set("abcdefghijklmnopqrstuvwxyz")
